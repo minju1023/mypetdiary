567f505a82a849cbffe96aa7a20aabf6e908d42c
minju102351 <minju102351@gmail.com>
2019-02-22 13:36:56 +0900

    반려동물 건강노트 메인함수기능

---
+++ application/Main.java
@@ -0,0 +1,31 @@
+package application;
+
+import Controller.RootController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader=new FXMLLoader(getClass().getResource("../View/login.fxml"));
+		Parent root=loader.load();
+		RootController rootController=loader.getController();
+		rootController.primaryStage=primaryStage;
+		
+		Scene scene=new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+
+	
+}
